1f8ea643d248e35eb71795d52acb9b83529c61c0
Keith Filkins <kafilkins@me.com>
2021-09-12 16:16:39 -0400

    commit tree 4741f4bb7100d5dbf4b1a281d8f5962dc84a2d60

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,27 @@
-# Implement your object-oriented solution here!
+class EvenFibonacci
+    attr_reader :fibo_array, :filtered_array, :sum
+  
+    def initialize(limit)
+      @first = 0
+      @second = 1
+      @fibo_array = []
+      @filtered_array = filtered_fibo_array(limit)
+      @sum = even_fibo_sum
+    end
+  
+    def filtered_fibo_array(limit)
+      i = 0
+      while i <= limit
+        i = @first + @second
+        break if i > limit
+        fibo_array << i if i.even?
+        @first = @second
+        @second = i
+      end
+      fibo_array
+    end
+  
+    def even_fibo_sum
+      filtered_array.reduce(:+) unless fibo_array.empty?
+    end
+  end
